9df262379a692e6b3965a19e066d71d06eada77f
AbhishekSharma079 <abhisheknawadih7@gmail.com>
2024-08-21 22:51:44 +0530

    first\

---
+++ script.js
@@ -0,0 +1,10 @@
+// document.getElementById('toggle-button').addEventListener('click', function() {
+//     var body = document.body;
+//     if(body.classList.contains('day-mode')) {
+//         body.classList.remove('day-mode');
+//         body.classList.add('night-mode');
+//     } else {
+//         body.classList.remove('night-mode');
+//         body.classList.add('day-mode');
+//     }
+// });
